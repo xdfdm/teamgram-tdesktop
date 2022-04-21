ef0a84f7960459383f2dcd07faeed32cc3f21c36
teamgram.io <wubenqi@gmail.com>
2022-04-21 23:17:39 +0800

    try fix client connects to tg server

--- Telegram/SourceFiles/mtproto/mtp_instance.cpp
+++ Telegram/SourceFiles/mtproto/mtp_instance.cpp
@@ -538,11 +538,11 @@ void Instance::Private::syncHttpUnixtime() {
 	if (base::unixtime::http_valid() || _httpUnixtimeLoader) {
 		return;
 	}
-	_httpUnixtimeLoader = std::make_unique<SpecialConfigRequest>([=] {
-		InvokeQueued(_instance, [=] {
-			_httpUnixtimeLoader = nullptr;
-		});
-	}, configValues().txtDomainString);
+	// _httpUnixtimeLoader = std::make_unique<SpecialConfigRequest>([=] {
+	// 	InvokeQueued(_instance, [=] {
+	//		_httpUnixtimeLoader = nullptr;
+	//	});
+	// }, configValues().txtDomainString);
 }
 
 void Instance::Private::restartedByTimeout(ShiftedDcId shiftedDcId) {
